1c4d6476cf0f4d140c706937aeee82c2be087bba
Elena Ishmukhametova <renbiwlen@gmail.com>
2026-04-26 23:08:36 +0300

    Исправить ошибки сборки

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./providers/router";
 //import { useTranslation } from "react-i18next"
-import { Header } from "../pages/main/layout/header/ui/Header";
 
 function App() {
   // const { t } = useTranslation("common");
