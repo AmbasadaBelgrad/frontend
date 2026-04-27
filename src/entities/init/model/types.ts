7bd79ee0f4227603a0145c3f79fbbfed7c71bda1
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-04-27 18:15:52 +0300

    Описать типы для /init

---
+++ src/entities/init/model/types.ts
@@ -0,0 +1,23 @@
+export type Language = {
+    code: string;
+    label: string;
+};
+
+export type Social = {
+    type: string;
+    url: string;
+};
+
+export type LegalLink = {
+    label: string;
+    url: string;
+};
+
+export type InitResponse = {
+    site_name: string;
+    seo_description?: string;
+    languages: Language[];
+    socials: Social[];
+    copyright: string;
+    legal_links: LegalLink[];
+};
