5c5581a2f6d7df489ec9a6a671f24390da4ff3b4
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-04-27 21:56:03 +0300

    Подключить QueryProvider

---
+++ src/app/providers/query/QueryProvider.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+type QueryProviderProps = {
+    children: React.ReactNode;
+};
+
+export const QueryProvider = ({ children }: QueryProviderProps) => {
+    const [queryClient] = React.useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        refetchOnWindowFocus: false,
+                    },
+                },
+            }),
+    );
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    )
+}
