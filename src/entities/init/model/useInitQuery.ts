3f3572b336bc7634a92dce932b72e671a3021316
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-04-27 21:39:03 +0300

    Сделать query-хук

---
+++ src/entities/init/model/useInitQuery.ts
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { useTranslation } from "react-i18next";
+import { getInit } from "../api/getInit";
+
+export const initQueryKey = (language: string) => ["init", language] as const;
+
+export function useInitQuery() {
+    const { i18n } = useTranslation();
+
+    const language = i18n.language || "ru";
+
+    return useQuery({
+        queryKey: initQueryKey(language),
+        queryFn: ({ signal }) => getInit(signal),
+        staleTime: Infinity,
+        gcTime: Infinity,
+        retry: 1,
+    });
+}
