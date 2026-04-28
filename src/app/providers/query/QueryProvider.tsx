fef702ebda260aca23468cc471905a3ff6e7e8e2
Liliya Kushnikova <iamlilkush4@gmail.com>
2026-04-28 10:42:17 +0300

    Создать SEO-хук

--- src/app/providers/query/QueryProvider.tsx
+++ src/app/providers/query/QueryProvider.tsx
@@ -2,24 +2,22 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 type QueryProviderProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 export const QueryProvider = ({ children }: QueryProviderProps) => {
-    const [queryClient] = React.useState(
-        () =>
-            new QueryClient({
-                defaultOptions: {
-                    queries: {
-                        refetchOnWindowFocus: false,
-                    },
-                },
-            }),
-    );
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
 
-    return (
-        <QueryClientProvider client={queryClient}>
-            {children}
-        </QueryClientProvider>
-    )
-}
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
--- src/entities/init/model/useInitQuery.ts
+++ src/entities/init/model/useInitQuery.ts
@@ -5,15 +5,15 @@ import { getInit } from "../api/getInit";
 export const initQueryKey = (language: string) => ["init", language] as const;
 
 export function useInitQuery() {
-    const { i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
-    const language = i18n.language || "ru";
+  const language = i18n.language || "ru";
 
-    return useQuery({
-        queryKey: initQueryKey(language),
-        queryFn: ({ signal }) => getInit(signal),
-        staleTime: Infinity,
-        gcTime: Infinity,
-        retry: 1,
-    });
+  return useQuery({
+    queryKey: initQueryKey(language),
+    queryFn: ({ signal }) => getInit(signal),
+    staleTime: Infinity,
+    gcTime: Infinity,
+    retry: 1,
+  });
 }
