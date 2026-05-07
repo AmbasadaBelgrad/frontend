3fab457d279a7fea5ade6fada7c51a10d314073b
Alexander Leontev <mr.leo.alexandr@yandex.ru>
2026-05-08 02:04:03 +0500

    feature/15_main_Page

--- src/pages/main/layout/MainLayout.tsx
+++ src/pages/main/layout/MainLayout.tsx
@@ -1,24 +1,8 @@
 import { Outlet } from "react-router-dom";
 import styles from "./MainLayout.module.css";
-import { useEffect, useState } from "react";
-import { apiClient } from "@shared/api/client.ts";
 
 // компонент будет оборачивать все маршруты в роутере
 const MainLayout = () => {
-  // example for request, need to add types - it is not a production version!
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [data, setData] = useState<any>(null);
-
-  useEffect(() => {
-    apiClient
-      .get("/api/v1/home")
-      .then((response) => {
-        setData(response);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-  }, []);
 
   return (
     <div className={styles.layout}>
@@ -28,7 +12,6 @@ const MainLayout = () => {
           <Outlet /> {/* Здесь подставляется содержимое страниц */}
         </div>
       </main>
-      {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
 
       {/* <Footer /> */}
     </div>
---
+++ src/pages/main/layout/ui/MainPage/MainPage.tsx
@@ -0,0 +1,37 @@
+import styles from "./MainPage.module.css";
+import { useEffect, useState } from "react";
+import { apiClient } from "@shared/api/client";
+import type { HomeResponse } from "@/pages/main/layout/ui/MainPage/types";
+
+const MainPage = () => {
+  const [data, setData] = useState<HomeResponse | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    apiClient
+      .get<HomeResponse>("/api/v1/home")
+      .then(setData)
+      .catch((err) => {
+        console.error(err);
+        setError("Ошибка загрузки");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) return <div>Загрузка...</div>;
+  if (error) return <div>{error}</div>;
+  if (!data) return null;
+
+  return (
+    <div className={styles.mainContainer}>
+      {/* HeroSection */}
+      {/* AboutSection */}
+      {/* TeamSection */}
+      {/* ProjectsSection */}
+      {/* ContactSection */}
+    </div>
+  );
+};
+
+export default MainPage;
