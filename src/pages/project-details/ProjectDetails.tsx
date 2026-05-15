373b9a0ab44461c08c07cf2995f0f1717b38e1b8
Rostbear61 <loshakovdmitry@yandex.ru>
2026-05-15 16:51:46 +0300

    feature(project_details): добавить моковые данные

--- src/pages/project-details/ProjectDetails.tsx
+++ src/pages/project-details/ProjectDetails.tsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { routesPaths } from "@shared/config/routesPaths.ts";
 import { useTranslation } from "react-i18next";
+//import { MainInfo } from "@pages/project-details/ui/MainInfo/";
+//import { InfoBlock } from "@pages/project-details/ui/InfoBlock/";
 import styles from "./ProjectDetails.module.css";
 
 export const ProjectDetails: React.FC = () => {
@@ -33,47 +35,47 @@ export const ProjectDetails: React.FC = () => {
 
   return (
     <div className={styles.container}>
-      <ul className={styles.pathList}>
-        <li className={styles.pathItem}>
-          <Link
-            className={styles.pathText}
-            aria-label={t("projectDetails.mainPage")}
-            to={routesPaths.home}
-          >
-            {t("projectDetails.mainPage")}
-          </Link>
-        </li>
-        <li className={styles.pathItem}>
-          <Link
-            className={styles.pathText}
-            aria-label={t("projectDetails.catalogProject")}
-            to={routesPaths.projects}
-          >
-            {!isMobile && t("projectDetails.catalogProject")}
-            {isMobile && "..."}
-          </Link>
-        </li>
-        <li className={styles.pathItem}>
-          <Link
-            className={styles.pathText}
-            aria-label={slug}
-            to={routesPaths.projectDetails}
-          >
-            {slug}
-          </Link>
-        </li>
-      </ul>
-      <Link
-        className={`${!isMobile ? "btn btn--primary" : ""} ${styles.button}`}
-        aria-label={t("projectDetails.textButtonDesktop")}
-        to={routesPaths.projects}
-      >
-        {!isTablet && !isMobile && t("projectDetails.textButtonDesktop")}
-        {isTablet && !isMobile && t("projectDetails.textButtonTablet")}
-        {isMobile && (
-          <img src="/back_arrow.svg" alt="arrow" className={styles.arrow} />
-        )}
-      </Link>
+      <div className={styles.navigation}>
+        <ul className={styles.pathList}>
+          <li className={styles.pathItem}>
+            <Link
+              className={styles.pathText}
+              aria-label={t("projectDetails.mainPage")}
+              to={routesPaths.home}
+            >
+              {t("projectDetails.mainPage")}
+            </Link>
+          </li>
+          <li className={styles.pathItem}>
+            <Link
+              className={styles.pathText}
+              aria-label={t("projectDetails.catalogProject")}
+              to={routesPaths.projects}
+            >
+              {!isMobile && t("projectDetails.catalogProject")}
+              {isMobile && "..."}
+            </Link>
+          </li>
+          <li className={styles.pathItem}>
+            <span className={styles.pathText}>{slug}</span>
+          </li>
+        </ul>
+        <Link
+          className={`${!isMobile ? "btn btn--primary" : ""} ${styles.button}`}
+          aria-label={t("projectDetails.textButtonDesktop")}
+          to={routesPaths.projects}
+        >
+          {!isTablet && !isMobile && t("projectDetails.textButtonDesktop")}
+          {isTablet && !isMobile && t("projectDetails.textButtonTablet")}
+          {isMobile && (
+            <img src="/back_arrow.svg" alt="arrow" className={styles.arrow} />
+          )}
+        </Link>
+      </div>
+      <div className={styles.content}>
+        {/* main info */}
+        {/* block info */}
+      </div>
     </div>
   );
 };
