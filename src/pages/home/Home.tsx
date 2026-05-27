e3c7672c870041edbba85b84819ef4a424ec973c
Elena Ishmukhametova <renbiwlen@gmail.com>
2026-05-28 02:14:54 +0300

    Стилизовать теги в первой карточке

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,64 @@
+// import React, { useState, useEffect } from "react";
+// import { Link } from "react-router-dom";
+// import { routesPaths } from "@shared/config/routesPaths.ts";
+// import { apiClient } from "@shared/api/client";
+// import { SectionProjects } from "./ui/section-projects";
+// import type { ProjectsPreview } from "./ui/section-projects/type";
+
+// type HomeResponse = {
+//   projects_preview: ProjectsPreview;
+// };
+
+// export const Home: React.FC = () => {
+//   const [homeResponse, setHomeResponse] = useState<HomeResponse | null>(null);
+
+//   useEffect(() => {
+//     apiClient
+//       .get<HomeResponse>("/api/v1/home")
+//       .then((response) => {
+//         setHomeResponse(response);
+//       })
+//       .catch((error) => {
+//         console.error("Error fetching home:", error);
+//       });
+//   }, []);
+
+//   if (!homeResponse) {
+//     return <div>Loading...</div>;
+//   }
+//   //  React.useEffect(() => {
+//   //     async function getHome() {
+//   //       try {
+//   //         const res = await apiClient.get<HomeResponse>("/home");
+  
+//   //         setHomeResponse(res);
+//   //       } catch (err) {
+//   //         console.log(err);
+//   //       }
+//   //     }
+//   //     getHome();
+//   //   }, []);
+  
+//   //   if (!homeResponse) {
+//   //     return null;
+//   //   }
+  
+
+//   return (
+//     <div>
+//       <h1>Главная страница</h1>
+//       <Link to={routesPaths.projects}>Проекты</Link>
+//       <SectionProjects projects_preview={homeResponse.projects_preview}></SectionProjects>
+//     </div>
+//   );
+// };
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { routesPaths } from "@shared/config/routesPaths.ts";
 import { apiClient } from "@shared/api/client";
-import { SectionProjects } from "./ui/section-projects";
+import { SectionProjects} from "./ui/section-projects";
 import type { ProjectsPreview } from "./ui/section-projects/type";
+
 
 type HomeResponse = {
   projects_preview: ProjectsPreview;
@@ -52,4 +107,6 @@ export const Home: React.FC = () => {
     </div>
   );
 };
+
+
 
--- src/pages/home/ui/section-projects/SectionProjects.tsx
+++ src/pages/home/ui/section-projects/SectionProjects.tsx
@@ -9,7 +9,7 @@ export const SectionProjects: React.FC<ProjectProps> = ({
   console.log(projects_preview);
   return (
     <section className={styles.container}>
-      <h2>{projects_preview.title}</h2>
+      <h2 className={styles.title}>{projects_preview.title}</h2>
       <ul className={styles.grid}>
         {projects_preview.items.map((project) => (
           <li
@@ -22,60 +22,69 @@ export const SectionProjects: React.FC<ProjectProps> = ({
               className={`${project.isFirst ? styles.image_first : styles.image_usual}`}
               loading="lazy"
             />
-            {project.action_button && (<div className={styles.line_diagonal}>
-              <svg
-                width="322"
-                height="540"
-                viewBox="0 0 322 540"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M9.73248 540H8.01286C1.84099 540 -2.00629 533.307 1.09985 527.974L306.281 3.97381C307.714 1.51337 310.347 0 313.194 0H313.706C319.872 0 323.72 6.68097 320.626 12.0143L16.6524 536.014C15.2212 538.481 12.5847 540 9.73248 540Z"
-                  fill="#CBF287"
-                  fill-opacity="0.5"
-                />
-              </svg>
-            </div>)}
+            {project.action_button && (
+              <div className={styles.line_diagonal}>
+                <svg
+                  width="322"
+                  height="540"
+                  viewBox="0 0 322 540"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M9.73248 540H8.01286C1.84099 540 -2.00629 533.307 1.09985 527.974L306.281 3.97381C307.714 1.51337 310.347 0 313.194 0H313.706C319.872 0 323.72 6.68097 320.626 12.0143L16.6524 536.014C15.2212 538.481 12.5847 540 9.73248 540Z"
+                    fill="#CBF287"
+                    fill-opacity="0.5"
+                  />
+                </svg>
+              </div>
+            )}
             <div
               className={`${project.isFirst ? styles.content_first : styles.content_usual}`}
             >
-              <div className={`${project.isFirst ? styles.content_container_first :""}`}>
-              {project.isFirst && (
-                <ul className={styles.tags}>
-                  {project.tags.map((tag) => (
-                    <li key={tag}>
-                      <span className={styles.tag}>{tag}</span>
-                    </li>
-                  ))}
-                </ul>
-              )}  
-              <h3
-                className={`${project.isFirst ? styles.title_first : styles.title_usual}`}
+              <div
+                className={`${project.isFirst ? styles.content_container_first : ""}`}
               >
-                {project.title}
-              </h3>
-              <p
-                className={`${project.isFirst ? styles.description_first : styles.description_usual}`}
-              >
-                {project.description}
-              </p>
-              {project.action_button && (
-                <Link
-                  to={project.action_button?.link.replace("{id}", project.id)}
-                  className={styles.button_project}
-                  aria-label={project.action_button?.label}
+                {project.isFirst && (
+                  <ul className={styles.tags}>
+                    {project.tags.map((tag) => (
+                      <li key={tag}>
+                        <span className={styles.tag}>{tag}</span>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+                <h3
+                  className={`${project.isFirst ? styles.title_first : styles.title_usual}`}
                 >
-                  <img
-                    src="/button for cards.svg"
-                    alt=""
-                    className={styles.icon}
-                  />
-                  <span className={styles.buttonText}>
-                    {project.action_button?.label}
-                  </span>
-                </Link>
-              )}
+                  {project.title}
+                </h3>
+                <p
+                  className={`${project.isFirst ? styles.description_first : styles.description_usual}`}
+                >
+                  {project.description}
+                </p>
+                {project.action_button && (
+                  <Link
+                    to={project.action_button.link.replace("{id}", project.id)}
+                    className={`
+    ${project.isFirst ? `btn btn--primary ${styles.button_project_first}` : `${styles.button_project}`}
+  `}
+                    aria-label={project.action_button.label}
+                  >
+                    {!project.isFirst && (
+                      <img
+                        src="/button for cards.svg"
+                        alt=""
+                        className={styles.icon}
+                      />
+                    )}
+
+                    <span className={styles.buttonText}>
+                      {project.action_button.label}
+                    </span>
+                  </Link>
+                )}
               </div>
             </div>
           </li>
