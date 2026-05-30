a7c00eeb4f23bea3c5bfe4a69170f3e071fb50b2
Alexander Leontev <mr.leo.alexandr@yandex.ru>
2026-05-30 11:02:01 +0500

    feature/17_section_about3

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { routesPaths } from "@shared/config/routesPaths.ts";
-import { AboutCommunity } from "./ui/AboutCommunity";
+import { AboutCommunity } from "./ui/AboutCommunity/AboutCommunity.tsx";
 
 export const Home: React.FC = () => {
   return (
