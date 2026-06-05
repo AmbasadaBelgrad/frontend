91afbb1badd20189f850747bb530dc36a44ca67b
Maksim <mkotyukov5@gmail.com>
2026-06-05 15:40:44 +0300

    fix: исправление после ревью

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,12 +1,12 @@
+import { useHomeQuery } from "@/entities/home/model/useHomeQuery";
 import { routesPaths } from "@shared/config/routesPaths.ts";
 import React from "react";
-import styles from "./Home.module.css";
-import { useHomeQuery } from "@/entities/home/model/useHomeQuery";
 import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 
 export const Home: React.FC = () => {
   const { data, isLoading, isError, error } = useHomeQuery();
-
+  throw new Error("Клик вызвал ошибку");
   if (isLoading) {
     return <div>Загрузка...</div>;
   }
