0f6085613b16f6ed3c114b792ed56baa2e3190f1
Максим <mkotyukov5@gmail.com>
2026-06-12 16:02:08 +0300

    feat(api): добавить константы URL для обращения к бэкенду согласно API контракту

--- src/entities/project/api/getCategories.ts
+++ src/entities/project/api/getCategories.ts
@@ -1,6 +1,7 @@
 import { apiClient } from "@/shared/api/client";
 import type { Category } from "../model/types";
+import { apiPaths } from "@/shared/api/config/apiPaths";
 
 export function getCategories(signal?: AbortSignal): Promise<Category[]> {
-  return apiClient.get<Category[]>("/projects/categories", { signal });
+  return apiClient.get<Category[]>(apiPaths.projects.categories, { signal });
 }
