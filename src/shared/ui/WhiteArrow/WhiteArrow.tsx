401ebd2ec154e1d8a659f0a9200a9c78f9a0c592
Dmitry Loshakov <loshakovdmitry@yandex.ru>
2026-07-15 14:31:14 +0300

    fix console error

--- src/shared/ui/WhiteArrow/WhiteArrow.tsx
+++ src/shared/ui/WhiteArrow/WhiteArrow.tsx
@@ -10,9 +10,9 @@ export const WhiteArrow = () => {
       <path
         d="M10 30H44M44 30L30 16M44 30L30 44"
         stroke="white"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
