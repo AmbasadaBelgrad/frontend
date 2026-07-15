eea43fa84a7341f2048c55dd9024c47c769efd0f
Rostbear61 <loshakovdmitry@yandex.ru>
2026-07-15 08:14:49 +0300

    add svg Arrow

---
+++ src/shared/ui/WhiteArrow/WhiteArrow.tsx
@@ -0,0 +1,19 @@
+export const WhiteArrow = () => {
+  return (
+    <svg
+      width="60"
+      height="60"
+      viewBox="0 0 60 60"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10 30H44M44 30L30 16M44 30L30 44"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};
