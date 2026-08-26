c444141c7221595aa81d65d44cbea3fe2da8d9f1
ambasadazaurbanizam <ambasadazaurbanizam@gmail.com>
2026-08-26 12:06:07 +0300

    feat: add support CTA to header

--- src/pages/main/layout/ui/header/Header.tsx
+++ src/pages/main/layout/ui/header/Header.tsx
@@ -153,6 +153,10 @@ export const Header = ({ data }: HeaderProps) => {
         </nav>
 
         <div className={styles.headerActions}>
+          <NavLink to={routesPaths.contacts} className={styles.supportLink}>
+            {t("header.menu.support")}
+          </NavLink>
+
           <div className={styles.langSwitcher} ref={langRef}>
             <button
               type="button"
@@ -275,6 +279,13 @@ export const Header = ({ data }: HeaderProps) => {
             {t(item.labelKey)}
           </NavLink>
         ))}
+        <NavLink
+          to={routesPaths.contacts}
+          className={styles.mobileSupportLink}
+          onClick={() => setIsMenuOpen(false)}
+        >
+          {t("header.menu.support")}
+        </NavLink>
       </div>
     </header>
   );
